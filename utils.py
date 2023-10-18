4d7fafc4e4a027656e60677c86eb56967cbbbdb6
KosenkoMichael <kosenko_michael_samara@mail.ru>
2023-10-18 17:47:35 +0400

    version for lab_2

--- utils.py
+++ utils.py
@@ -1,5 +1,5 @@
 import os
-import csv 
+import csv
 from typing import List
 
 import requests
@@ -13,7 +13,8 @@ def parse(year_from: int, year_to: int, step: int = 1) -> List[List[str]]:
         for month in range(1, 13):
             URL = f"https://www.gismeteo.ru/diary/4618/{year}/{month}/"
             print(f"new url is open: {year}.{month}")
-            html_page = requests.get(URL, headers={"User-Agent": "Mozilla/5.0"})
+            html_page = requests.get(
+                URL, headers={"User-Agent": "Mozilla/5.0"})
             soup = BeautifulSoup(html_page.text, "lxml")
             for day in soup.find_all("td", class_="first"):
                 try:
@@ -33,14 +34,12 @@ def parse(year_from: int, year_to: int, step: int = 1) -> List[List[str]]:
                     )
                     all_data.append(
                         [
-                            day.text + "." + str(month).zfill(2) + "." + str(year),
-                            "day",
-                            temp.text + "°C",
-                            press.text + "мм.рт.ст.",
+                            day.text, str(month), str(year),
+                            temp.text,
+                            press.text,
                             wind.text,
-                            "night",
-                            night_temp.text + "°C",
-                            night_press.text + "мм.рт.ст.",
+                            night_temp.text,
+                            night_press.text,
                             night_wind.text,
                         ]
                     )
@@ -56,13 +55,13 @@ def parse(year_from: int, year_to: int, step: int = 1) -> List[List[str]]:
     return all_data
 
 
-def csv_update(data: List[List[str]]) -> None:
-    path = os.path.join("test_dataset.csv")
+def csv_update(path: str, data: List[List[str]]) -> None:
     with open(path, "w", encoding="utf-8", newline="") as file:
         writer = csv.writer(file)
         writer.writerows(data)
 
 
 def main() -> None:
-    csv_update(parse(2007, 2008))
+    csv_update("dataset.csv", parse(2008, 2023))
+    csv_update("test_dataset.csv", parse(2008, 2009))
     print("Загрузка завершена")
